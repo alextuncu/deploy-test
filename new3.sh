5900898d86c60c3a7f52e99918b9128b59444549
Alex T <61549478+alextuncu@users.noreply.github.com>
2022-12-06 14:08:50 +0200

    Add files via upload

---
+++ new3.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# Set variable values before proceeding. 
+
+# Variables
+sourceResourceGroup= WG-Test
+sourceSubscription= b530a69c-8dba-430f-9118-72d714d954b9
+vmName= WG-VM
+
+destinationResourceGroup= mig-vm-test-01
+destinationSubscription= 2b80fbe4-9bdb-4152-883b-3304f3369d02
+
+# Set your current subscription for the source virtual machine
+az account set --subscription $sourceSubscription
+
+# Load variables about your virtual machine
+# osType = windows or linux
+osType=$(az vm get-instance-view --resource-group $sourceResourceGroup \
+    --name $vmName --subscription $sourceSubscription \
+    --query 'storageProfile.osDisk.osType' --output tsv)
+
+# offer = Your offer in Marketplace
+offer=$(az vm get-instance-view --resource-group $sourceResourceGroup \
+    --name $vmName --query 'storageProfile.imageReference.offer' --output tsv)
+
+# plan = Your plan in Marketplace
+plan=$(az vm get-instance-view --resource-group $sourceResourceGroup \
+    --name $vmName --query 'plan' --output tsv)
+
+# publisher = Your publisher in Marketplace
+publisher=$(az vm get-instance-view --resource-group $sourceResourceGroup \
+    --name $vmName --query 'storageProfile.imageReference.publisher' --output tsv)
+
+# Get information to create new virtual machine
+planName=$(az vm get-instance-view --resource-group $sourceResourceGroup \
+    --subscription $sourceSubscription --query 'plan.name' --name $vmName)
+planProduct=$(az vm get-instance-view --resource-group $sourceResourceGroup \
+    --subscription $sourceSubscription --query 'plan.product' --name $vmName)
+planPublisher=$(az vm get-instance-view --resource-group $sourceResourceGroup \
+    --subscription $sourceSubscription --query 'plan.publisher' --name $vmName)
+
+# Get the name of the OS disk
+osDiskName=$(az vm show --resource-group $sourceResourceGroup --name $vmName \
+    --query 'storageProfile.osDisk.name' --output tsv)
+
+# Verify the terms for your market virtual machine
+az vm image terms show --offer $offer --plan '$plan' --publisher $publisher \
+    --subscription $sourceSubscription
+
+# Deallocate the virtual machine
+az vm deallocate --resource-group $sourceResourceGroup --name $vmName
+
+# Create a snapshot of the OS disk
+az snapshot create --resource-group $sourceResourceGroup --name MigrationSnapshot \
+    --source "/subscriptions/$sourceSubscription/resourceGroups/$sourceResourceGroup/providers/Microsoft.Compute/disks/$osDiskName"
+
+# Move the snapshot to your destination resource group
+az resource move --destination-group $destinationResourceGroup \
+    --destination-subscription-id $destinationSubscription \
+    --ids "/subscriptions/$sourceSubscription/resourceGroups/$sourceResourceGroup/providers/Microsoft.Compute/snapshots/MigrationSnapshot"
+
+# Set your subscription to the destination value
+az account set --subscription $destinationSubscription
+
+# Accept the terms from the Marketplace
+az vm image terms accept --offer $offer --plan '$plan' --publisher $publisher \
+    --subscription $destinationSubscription
+
+# Create disk from the snapshot 
+az disk create --resource-group $destinationResourceGroup --name DestinationDisk \
+    --source "/subscriptions/$destinationSubscription/resourceGroups/$destinationResourceGroup/providers/Microsoft.Compute/snapshots/MigrationSnapshot" \
+    --os-type $osType
+
+# Create virtual machine from disk
+az vm create --resource-group $destinationResourceGroup --name $vmName \
+    --plan-name $planName --plan-product $planProduct  --plan-publisher $planPublisher \
+    --attach-os-disk "/subscriptions/$destinationSubscription/resourceGroups/$destinationResourceGroup/providers/Microsoft.Compute/disks/DestinationDisk" \
+    --os-type $osType
